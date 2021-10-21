30440842a431a045c564a07cd77a34668bd33f49
nicknovoa03 <nicknovoa03@gmail.com>
2021-10-21 16:35:40 -0500

    Styled UI

--- src/SideScreen.js
+++ src/SideScreen.js
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -12,6 +11,7 @@ import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import HttpsIcon from '@mui/icons-material/Https';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 
 import menacePng from './MenaceSamples/1.png'
 
@@ -28,12 +28,46 @@ function Copyright(props) {
   );
 }
 
+function valueText(value){
+  return `${value}`;
+}
+
 const theme = createTheme();
 
-function valueText(value){
-    return `${value}`;
-}
-export default function MenaceMint() {
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  typography: {
+    fontFamily: [
+      'Open Sans',
+      'Roboto'
+    ].join(','),
+  }
+});
+
+const MintButton = styled(Button)({
+  fontSize: 16,
+  padding: '6px 12px',
+  border: '1px solid',
+  lineHeight: 1.5,
+  backgroundColor: 'grey',
+    fontFamily: [
+    'Roboto',
+  ].join(','),
+  '&:hover': {
+    backgroundColor: '#fff',
+    borderColor: 'rgba(255, 255, 255, 0.08)',
+    boxShadow: 'rgba(255, 255, 255, 0.16)',
+  },
+  '&:active': {
+    boxShadow: '#fff',
+    backgroundColor: '#fff',
+    borderColor: '#fff',
+  }
+});
+
+export default function SideScreen() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -44,16 +78,16 @@ export default function MenaceMint() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={darkTheme}>
       <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
         <Grid
           item
           xs={false}
-          sm={4}
+          sm={3}
           md={7}
           sx={{
-            backgroundImage: menacePng,
+            backgroundImage: `url(${menacePng})`,
             backgroundRepeat: 'no-repeat',
             backgroundColor: 'Transparent',
             backgroundSize: 'cover',
@@ -61,7 +95,7 @@ export default function MenaceMint() {
           }}
         />
 
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} sx={{backgroundColor:'Grey'}} Square>
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={5} sx={{backgroundColor:'background.paper'}} Square>
             <Box
                 sx={{
                 my: 2,
@@ -71,15 +105,15 @@ export default function MenaceMint() {
                 alignItems: 'center',
                 }}
             >
-                <Fab sx={{ my:4 }} variant="extended" size="Medium" color="Black" aria-label="add">
+                <Fab sx={{ my:4 }} variant="extended" size="Medium" color="action.active" aria-label="add">
                     <AccountBalanceWalletIcon sx={{ mr: 1 }} />
                     Connect Wallet
                 </Fab>
-                <Avatar sx={{ m:2, bgcolor: 'Black' }}>
-                    <HttpsIcon />
+                <Avatar color='text.primary' sx={{ m:2}}>
+                    <HttpsIcon color='text.secondary'/>
                 </Avatar>
-                <Typography component="h1" variant="h5">
-                    Mint Menace
+                <Typography component="h1" variant="Title" fontWeight="fontWeightBold" color="White">
+                    MINT MENACE
                 </Typography>
                 <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
                 <Slider
@@ -91,15 +125,16 @@ export default function MenaceMint() {
                     marks
                     min={0}
                     max={2}
+                    sx={{
+                      color:'text.primary'
+                    }}
                 />
-                <Button
-                    type="submit"
+                <MintButton
                     fullWidth
                     variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
                     >
                     Mint
-                </Button>
+                </MintButton>
                 <Copyright sx={{ mt: 1 }} />
             </Box>
           </Box>
