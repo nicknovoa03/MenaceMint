57a915a50ff9290b38fe60eaf38081b8f78895c4
nicknovoa03 <nicknovoa03@gmail.com>
2021-12-01 20:43:11 -0600

    DNS dapp

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Whitelist from './AddToWhitelistHook'
 
 ReactDOM.render(
   <React.StrictMode>
-      <HookApp />
+      <Whitelist />
   </React.StrictMode>,
   document.getElementById('root')
 );
