d3f68511df9633de0ddca3c539887878f24a83ba
Nils R. Garland <nils.r.garland@gmail.com>
2021-12-07 03:10:40 +0100

    Changes.

--- src/FullScreenHook.js
+++ src/FullScreenHook.js
@@ -11,30 +11,94 @@ import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 import Slider from '@mui/material/Slider';
 import MintBackground from './MenaceSamples/MintBackground.jpg'
 
-import AsyncConnect from './AsyncConnect';
+// import AsyncConnect from './AsyncConnect';
 
-import WhitelistDictionary from './WhitelistDictionary'
+import WhitelistDictionary from './WhitelistDictionary';
+import { contractAddr, contract } from './Contract';
 
 import { ethers } from 'ethers';
 import { parseEther } from '@ethersproject/units';
+import Web3 from "web3";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
 
 function FullScreenHook() {
 
-    const [wallet, menaceContract, menaceAddress, web3] = AsyncConnect();
-    const [mintAmount, setMintAmount] = useState();
-    const whitelist = WhitelistDictionary();
+    const [mintAmount, setMintAmount] = useState(1);
+    const [wallet, setWallet] = useState();
+    const [web3, setWeb3] = useState();
+    // const whitelist = WhitelistDictionary();
 
-    async function presaleMint(mintAmount, wallet) {
-        var price = ethers.utils.parseEther("0.09")['_hex'] * mintAmount
-        // call transfer function
-        menaceContract.methods.mintMenaceWhitelist(mintAmount.toString()).send({ from: wallet, value: price })
+    const providerOptions = {
+        injected: {
+            package: null
+        },
+        walletconnect: {
+            package: WalletConnectProvider,
+            network: "ethereum",
+            options: {
+                infuraId: "bad8cc770bef49dc88683bf2290205c8" // required
+            }
+        }
+    };
+
+    const web3Modal = new Web3Modal({
+        network: "mainnet",
+        cacheProvider: true,
+        providerOptions
+    });
+
+    function compressedAddr(addr) {
+        if (addr) {
+            let first_account_chars = addr.substring(0, 5);
+            let last_account_chars = addr.substring(addr.length - 4, addr.length);
+            return first_account_chars + "..." + last_account_chars;
+        }
     }
 
-    async function mint(mintAmount, wallet) {
-        var price = ethers.utils.parseEther("0.2")['_hex'] * mintAmount
-        // call transfer function
-        menaceContract.methods.mintMenace(mintAmount.toString()).send({ from: wallet, value: price })
+    async function connect() {
+        try {
+            let provider;
+            provider = await web3Modal.connect();
+            let web3 = new Web3(provider);
+            setWeb3(web3);
+            web3.eth.getAccounts().then(async (addr) => {
+                setWallet(addr[0]);
+            });
+        } catch (e) {
+            console.log(e);
+            return;
+        }
+    }
+
+    async function presaleMint() {
+        const price = 0.09 * mintAmount;
+        const mintable_price = price.toString();
+        
+        const tx = {
+            from: wallet,
+            to: contractAddr,
+            value: ethers.utils.parseEther(mintable_price)["_hex"],
+            data: contract.methods.mintMenaceWhitelist(mintAmount.toString()).encodeABI(),
+        };
+
+        const createTransaction = await web3.eth.sendTransaction(tx);
+
+    }
+
+    async function mint() {
+        const price = 0.2 * mintAmount;
+        const mintable_price = price.toString();
+
+        const tx = {
+            from: wallet,
+            to: contractAddr,
+            value: ethers.utils.parseEther(mintable_price)["_hex"],
+            data: contract.methods.mintMenace(mintAmount.toString()).encodeABI(),
+        };
+
+        const createTransaction = await web3.eth.sendTransaction(tx);
     }
 
     function handleSlider(event, value) {
@@ -42,11 +106,6 @@ function FullScreenHook() {
         setMintAmount(value)
     }
 
-    async function handleMint(event) {
-        event.preventDefault();
-        presaleMint(mintAmount,wallet)
-
-    }
     const darkTheme = createTheme({
         palette: {
             mode: 'dark',
@@ -72,7 +131,7 @@ function FullScreenHook() {
         );
     }
 
-    const MintButton = styled(Button)({
+    const Web3Button = styled(Button)({
         fontSize: 16,
         padding: '6px 12px',
         border: '1px solid',
@@ -169,14 +228,23 @@ function FullScreenHook() {
                                     color: 'text.primary'
                                 }}
                             />
-                            <MintButton
+                            { wallet &&
+                            <Web3Button
                                 fullWidth
                                 variant="contained"
-                                type="submit"
-                                onClick={(event) => handleMint(event)}
+                                onClick={presaleMint}
                             >
                                 Mint
-                            </MintButton>
+                            </Web3Button>
+                            } {!wallet &&
+                            <Web3Button
+                                fullWidth
+                                variant="contained"
+                                onClick={() => connect()}
+                            >
+                                Connect
+                            </Web3Button>
+                            }
                             <Typography
                                 component="h4"
                                 variant="Subtitle"
@@ -188,7 +256,16 @@ function FullScreenHook() {
                                     m: 1
                                 }}
                             >
-                                Connected Wallet: {wallet}
+                            {wallet &&
+                                <>
+                                    Connected Wallet: {compressedAddr(wallet)}
+                                </>
+                            } {!wallet &&
+                                <>
+                                    Please connect to your wallet.
+                                </>
+                            }
+                                
                             </Typography>
                             <Typography
                                 component="h4"
@@ -201,7 +278,7 @@ function FullScreenHook() {
                                     m: 1
                                 }}
                             >
-                                Contract Address: {menaceAddress}
+                                Contract Address: {contractAddr}
                             </Typography>
                             <Copyright sx={{ mt: 1 }} />
                         </Box>
